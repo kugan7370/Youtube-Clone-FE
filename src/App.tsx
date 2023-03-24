ecadb359b99692f448ed3498d1b5f083d3c8f794
Kugan Priyan <kugapriyanj97@gmail.com>
2023-03-24 12:39:29 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import SideBar from "./Components/SideBar";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-     <h1>welcome</h1>
+    <div className="flex flex-col h-screen">
+      <Navbar />
+
+      <div className="flex flex-1">
+        <div className="w-[200px] bg-blue-500">
+        <SideBar />
+        </div>
+
+        <main className=" bg-red-400 flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-full'>
+      home
+    </div>
+  )
+}
+
+export default Home
