63a3f9ec55d23a2b0f42b37e32749fe32d8ecd21
Kugan Priyan <kugapriyanj97@gmail.com>
2023-06-13 19:43:21 +0530

    fixed cookies bug

--- src/Utils/Token.ts
+++ src/Utils/Token.ts
@@ -1,6 +1,9 @@
-export const setCookieToken = (token: string) => {
-    document.cookie = `access_token=${token}`
 
+
+
+export const setCookieToken = (token: string) => {
+    const expirationDate = new Date(Date.now() + 86400000);
+    document.cookie = `access_token=${token} Secure; SameSite=None; domain=${process.env.REACT_APP_DOMAIN}; path=/ ; expires=${expirationDate.toUTCString()}`
 }
 
 
--- src/Utils/host.ts
+++ src/Utils/host.ts
@@ -1,1 +1,2 @@
 export const Host = process.env.REACT_APP_HOST_URL
+export const Domain = process.env.REACT_APP_DOMAIN
