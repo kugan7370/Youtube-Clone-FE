04b6cbe5a69efbcfc30a0383c542fd66442033f3
Kugan Priyan <kugapriyanj97@gmail.com>
2023-08-30 02:32:08 +0530

    added responsive

--- src/Components/RecomandedVideoCard.tsx
+++ src/Components/RecomandedVideoCard.tsx
@@ -6,11 +6,11 @@ import moment from 'moment'
 
 function RecomandedVideoCard({ videoData }: { videoData: HomeVideos }) {
     let title = videoData.title
-    title = title.length > 50 ? title.slice(0, 50) + '...' : title
+    title = title.length > 25 ? title.slice(0, 25) + '...' : title
 
     return (
         <div className='flex w-full gap-5'>
-            <NavLink to={`/details/${videoData._id}`} className="h-36 w-60 rounded-2xl overflow-hidden bg-red-300">
+            <NavLink to={`/details/${videoData._id}`} className="h-24 w-40 rounded-2xl overflow-hidden bg-red-300">
                 <img className='w-full h-full object-cover' src={videoData.thumbnail} alt="img" />
             </NavLink>
 
@@ -18,7 +18,7 @@ function RecomandedVideoCard({ videoData }: { videoData: HomeVideos }) {
                 <h1 className='text-sm font-normal'>{title}</h1>
                 <h1 className='text-sm text-gray-500 mt-1'>{videoData.userDetails.username}</h1>
                 <h1 className='text-sm text-gray-500 mt-2'>{videoData.views} views</h1>
-                <h1 className='text-sm  text-gray-500'>{moment(videoData.createdAt).fromNow()}</h1>
+                {/* <h1 className='text-sm  text-gray-500'>{moment(videoData.createdAt).fromNow()}</h1> */}
 
             </div>
 
